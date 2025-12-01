bfe86d01ec049fb9b296f2e64b0368aacc6a71c3
JNM <151471955+DarkM00n9@users.noreply.github.com>
2025-12-01 20:45:23 +0100

    Add company relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function company(): BelongsTo
+{
+    return $this->belongsTo(Company::class);
+}
 
     /**
      * The attributes that are mass assignable.
